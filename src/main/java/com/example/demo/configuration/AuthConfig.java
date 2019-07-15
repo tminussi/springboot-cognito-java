1873c8aac74ae5e6868d428ac07b70f0f69c6bf5
Thales Sousa <tminussi@gmail.com>
2019-07-15 13:40:26 +0200

    Update AuthConfig.java

--- src/main/java/com/example/demo/configuration/AuthConfig.java
+++ src/main/java/com/example/demo/configuration/AuthConfig.java
@@ -19,15 +19,29 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new AuthFilter(processor, authenticationManager()))
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
     }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // AuthenticationTokenFilter will ignore the below paths
+        web
+                .ignoring()
+                .antMatchers(HttpMethod.GET, "/auth/**");
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/auth/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilter(new AuthFilter(processor, authenticationManager()))
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//    }
 
     public ConfigurableJWTProcessor<SecurityContext> getProcessor() {
         return this.processor;
